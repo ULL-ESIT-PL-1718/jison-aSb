7993d39e5b0ceb8a56731bbf069dcbe975e60e7e
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-03-15 15:32:22 +0000

    preparing for browser debugging

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("button").click(function () {
+    try {
+      var result = aSb.parse($("input").val())
+      $("#output").html(result);
+    } catch (e) {
+      $("#output").html(String(e));
+    }
+  });
+});
